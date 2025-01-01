ccb348f19caa9bd29b4bed9af0aa88178ce23a54
john <NyanSatan@users.noreply.github.com>
2025-01-01 21:37:18 +0100

    Redesign-2025 iteration 3 (mostly APT)

--- apt/depictions/js/css.js
+++ apt/depictions/js/css.js
@@ -1,12 +1,12 @@
 function setCSS() {
-	var iOSVersion = getiOS();
+    var iOSVersion = getiOS();
     
     var css;
     
     if (iOSVersion < 7) {
-        css = 'css/skeuomorphic.css';
+        css = '/apt/css/skeuomorphic.css';
     } else {
-        css = 'css/minimalistic.css';
+        css = '/apt/css/minimalistic.css';
     }
     
     var head  = document.getElementsByTagName('head')[0];
@@ -19,15 +19,15 @@ function setCSS() {
 }
 
 function getiOS() {
-	var userAgent = navigator.userAgent
+    var userAgent = navigator.userAgent
     var OS = 'OS';
-    
-	if (!userAgent.match('iPhone') && !userAgent.match('iPod') && !userAgent.match('iPad')) {
-		return 'unknown';
-	}
+
+    if (!userAgent.match('iPhone') && !userAgent.match('iPod') && !userAgent.match('iPad')) {
+        return 'unknown';
+    }
 
     var versionOffset = userAgent.indexOf(OS) + OS.length + 1;
     var underscoreOffset = userAgent.indexOf('_', versionOffset);
-    
+
     return parseInt(userAgent.substring(versionOffset, underscoreOffset));
 }
--- apt/update.sh
+++ apt/update.sh
@@ -3,5 +3,5 @@
 set -e
 
 rm -rf Packages Packages.bz2
-dpkg-scanpackages -m . /dev/null >Packages
+dpkg-scanpackages -m . /dev/null > Packages
 bzip2 -k -z Packages
