d735646535352fcc54adc83957b211d5faa3d1d7
john <NyanSatan@users.noreply.github.com>
2025-10-25 04:55:00 +0200

    Move things around

--- js/index.js
+++ js/index.js
@@ -46,35 +46,6 @@ function templateCallback(xhr, tab) {
     }
 }
 
-function iOS() {
-    return !!navigator.platform.match(/iPhone|iPod|iPad/);
-}
-
-var colors = [
-    "#47234d",
-    "#001f3f",
-    "#06402b",
-    "#91043c"
-];
-
-function setBackground() {
-    var timestamp = Math.floor(Date.now() / 1000);
-    var color = colors[timestamp % colors.length];
-
-    var gradient = "radial-gradient(circle at bottom, " + color + ", black)";
-    var bodyElement = document.body;
-
-    if (!iOS()) {
-        bodyElement.style["background"] = gradient;
-        bodyElement.style["background-attachment"] = "fixed";
-    } else {
-        var bgElement = document.createElement("div");
-        bgElement.className = "fixed-bg";
-        bgElement.style["background"] = gradient;
-        bodyElement.prepend(bgElement);
-    }
-}
-
 function main() {
     setBackground();
 
